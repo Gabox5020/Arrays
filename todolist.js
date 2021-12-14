e8d8d71478b9e18935cc2d8a123cd8934fc02743
Gabriel <gaboxroll.2@gmail.com>
2021-12-14 12:45:44 -0300

    tercer click

--- todolist.js
+++ todolist.js
@@ -1,12 +1,14 @@
 window.onload = iniciar;
+var tareas = [];
 
-function iniciar() {
+  function iniciar() {
     var btnAgregar = document.getElementById("btnAgregar");
     btnAgregar.addEventListener("click", clickBtnAgregar)
 }
 
-function clickBtnAgregar() {
-   var txtTarea = document.getElementById("txtTarea");
-   var tarea = txtTarea.value;
-    alert(tarea)
+  function clickBtnAgregar() {
+     var txtTarea = document.getElementById("txtTarea");
+     var tarea = txtTarea.value;
+       tareas.push(tarea);
+       alert(tareas)
 }
